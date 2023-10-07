988c32f88a60167175bb98f0b8c320b10d31e5ac
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:52:29 -0400

    Delete iPads Cleanup.py

--- iPads Cleanup.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('DevicesWithInventory.csv')
-df['Device name'].fillna('', inplace=True)
-# Filter the dataframe to keep only rows where 'Device name' starts with 'EXAMPLE-' -- filter this for the Intune group you're looking for
-df = df[df['Device name'].str.startswith('EXAMPLE-')]
-df.to_excel('C:/folder/EXAMPLE.xlsx', index=False)
