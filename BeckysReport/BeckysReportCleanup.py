a0b3e4bfc2cece038784db22ebd5fc77d17e30e3
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:59:22 -0400

    Update BeckysReportCleanup.py

--- BeckysReport/BeckysReportCleanup.py
+++ BeckysReport/BeckysReportCleanup.py
@@ -1,11 +1,9 @@
-# this script is supposed to run after running BeckysReport.ps1
+# this script is supposed to run after running ad-report.ps1
 import pandas as pd
 import datetime
-  
-# reading beckys csv file
-df = pd.read_csv('BeckysReport.csv')
 
-# updating the column value/data
+df = pd.read_csv('ad-report.csv')
+
 df.loc[df['Role'] == 512, 'Role'] = ''
 df.loc[df['Role'] == 514, 'Role'] = 'DISABLED'
 df.loc[df['Role'] == 544, 'Role'] = 'NO_PWD_REQD'
@@ -16,6 +14,5 @@ df.loc[df['Role'] == 66050, 'Role'] = 'DISABLED^NO_PWD_EXP'
 df.loc[df['Role'] == 66080, 'Role'] = 'NO_PWD_EXP^NO_PWD_REQD'
 df.loc[df['Role'] == 66082, 'Role'] = 'DISABLED^NO_PWD_EXP^NO_PWD_REQD'
 
-# export the file as .xlsx with timestamp
 e = datetime.date.today()
-df.to_excel(f"BeckysReport_{e}.xlsx", sheet_name="Quarterly Report", index=False)
+df.to_excel(f"ad-report_{e}.xlsx", sheet_name="Quarterly Report", index=False)
