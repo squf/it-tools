71710a93b199a19074c056ec5ed92b335951b4d8
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:52:57 -0400

    Delete AddADUserToGroup/AddADUserToGroup.ps1

--- AddADUserToGroup/AddADUserToGroup.ps1
+++
@@ -1,3 +0,0 @@
-﻿Import-Module ActiveDirectory 
-
-Import-Csv -Path “C:\scripts\AddADUserToGroup\Users-To-Add.csv” | ForEach-Object {Add-ADGroupMember -Identity “Management-11720187724” -Members $_.’User-Name’}
