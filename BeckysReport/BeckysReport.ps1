d274313191c9b3998b53a3bb3b44398078feb15f
squf <1562592+squf@users.noreply.github.com>
2023-10-06 23:01:07 -0400

    Update BeckysReport.ps1

--- BeckysReport/BeckysReport.ps1
+++ BeckysReport/BeckysReport.ps1
@@ -1,35 +1,19 @@
-﻿# Split path
-$Path = Split-Path -Parent "C:\scripts\BeckysReport\*.*"
-
-# Create variable for the date stamp in log file
+﻿$Path = Split-Path -Parent "C:\folder\*.*"
 $LogDate = Get-Date -f yyyyMMddhhmm
-
-# Define CSV and log file location variables
-# They have to be on the same location as the script
-$Csvfile = $Path + "\BeckysReport.csv"
-
-# Import Active Directory module
+$Csvfile = $Path + "\ad-report.csv"
 Import-Module ActiveDirectory
 
-# Set distinguishedName as searchbase, you can use one OU or multiple OUs
-# Or use the root domain like DC=exoip,DC=local
 $DNs = @(
-    "OU=Millerpipeline,OU=artera.com,DC=corp,DC=artera,DC=com"
+    "OU=example,OU=company.com,DC=corp,DC=company,DC=com"
 
 )
 
-# Create empty array
 $AllADUsers = @()
-
-# Loop through every DN
 foreach ($DN in $DNs) {
     $Users = Get-ADUser -resultpagesize 125 -SearchBase $DN -Filter * -Properties * 
-
-    # Add users to array
     $AllADUsers += $Users
 }
 
-# Create list
 $AllADUsers | Sort-Object Name | Select-Object `
 @{Label = "Directory Name"; Expression = { $_.cn } },
 @{Label = "Display Name"; Expression = { $_.DisplayName } },
@@ -39,5 +23,4 @@ $AllADUsers | Sort-Object Name | Select-Object `
 @{Label = "NoExpire"; Expression = { $_.PasswordNeverExpires } },
 @{Label = "Role"; Expression = { $_.UserAccountControl } }|
 
-# Export report to CSV file
-Export-Csv -Encoding UTF8 -Path $Csvfile -NoTypeInformation #-Delimiter ";"
+Export-Csv -Encoding UTF8 -Path $Csvfile -NoTypeInformation #-Delimiter ","
