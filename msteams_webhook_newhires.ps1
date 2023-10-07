ddbdb040b291abff4375f8bf380dbae4e07579f6
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:24:47 -0400

    Update and rename whatdoesthisthingdo.ps1 to msteams_webhook_newhires.ps1

--- msteams_webhook_newhires.ps1
+++ msteams_webhook_newhires.ps1
@@ -1,17 +1,13 @@
-﻿# Teams webhook URL
-$uri = "https://arteraservices.webhook.office.com/webhookb2/4127f798-3092-4654-9393-8aa34ed4de78@21802c71-f78d-4116-a81f-c204aaadc19a/IncomingWebhook/4620881f164042fab5ed79c3d1be13b0/903d861d-c34a-4dc1-945a-15a621092eb5"
+﻿# replace this URI with your webhook link from whatever app you're setting up a webhook in (Microsoft Teams in this case)
+$uri = "https://examplecompany.webhook.office.com/webhookb2/..."
+$ItemImage = 'example.png'
 
-# Image on the left-hand side, a regular user picture
-$ItemImage = 'https://em-content.zobj.net/source/microsoft-teams/363/construction-worker_1f477.png'
 
-# Get the current time and calculate the 1 hour threshold
 $currentTime = Get-Date
 $creationThreshold = $currentTime.AddHours(-1)
-
 $NewUsersTable = New-Object 'System.Collections.Generic.List[System.Object]'
 $ArrayTable = New-Object 'System.Collections.Generic.List[System.Object]'
 
-# Retrieve user objects created within the last hour in the specified OU
 $users = Get-ADUser -Filter * -SearchBase "OU=Millerpipeline,OU=artera.com,DC=corp,DC=artera,DC=com" -SearchScope Subtree -Properties WhenCreated | Where-Object { $_.WhenCreated -ge $creationThreshold }
 foreach ($user in $users) {
     Write-Host "Working on $($user.Name)" -ForegroundColor White
