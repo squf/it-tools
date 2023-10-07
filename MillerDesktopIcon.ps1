bb2b83bfefd629f4093fb250786fd9e857c6373c
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:42:00 -0400

    Update MillerDesktopIcon.ps1

--- MillerDesktopIcon.ps1
+++ MillerDesktopIcon.ps1
@@ -1,20 +1,15 @@
-﻿#Variables creating local folder and download .ico file
-$LocalIconFolderPath = "C:\Program Files (x86)\Microsoft\Edge\Application"
+﻿$LocalIconFolderPath = "C:\Program Files (x86)\Microsoft\Edge\Application"
 $SourceIcon = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 $DestinationIcon = "C:\Program Files (x86)\Microsoft\Edge\Application"
 
-
-#Step 1 - Create a folder to place the URL icon
 New-Item $LocalIconFolderPath -Type Directory
 
-#Step 2 - Download a ICO file from a website into previous created folder
 curl $SourceIcon -o $DestinationIcon
 
-#Step 3 - Add the custom URL shortcut to your Desktop with custom icon
 $new_object = New-Object -ComObject WScript.Shell
 $destination = $new_object.SpecialFolders.Item('AllUsersDesktop')
-$source_path = Join-Path -Path $destination -ChildPath '\\Miller Email.lnk'
+$source_path = Join-Path -Path $destination -ChildPath '\\Email.lnk'
 $source = $new_object.CreateShortcut($source_path)
-$source.TargetPath = 'https://outlook.com/artera.com'
+$source.TargetPath = 'https://outlook.com'
 $source.IconLocation = ”C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe”
 $source.Save()
