465ca38e07d82e9625d4af00d773e7dd696cd0d7
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:16:06 -0400

    Update UpdateComputerDesc.ps1

--- UpdateComputerDesc.ps1
+++ UpdateComputerDesc.ps1
@@ -1,18 +1,11 @@
 Import-Module ActiveDirectory
 
-# Replace with the path to your CSV file
 $csvPath = "C:\scripts\computer_displaynames_serials.csv"
-
-# Import the CSV file
 $csv = Import-Csv $csvPath
-
-# Loop through each row in the CSV
 foreach ($row in $csv) {
-    # Get the computer object from Active Directory
     $computer = Get-ADComputer -Identity $row.Hostname
 
     if ($computer) {
-        # Update the Description attribute with the user's display name and computer's serial number
         $description = "Assigned to user: $($row.DisplayName) | Serial Number: $($row.SerialNumber)"
         Set-ADComputer -Identity $computer -Description $description
         Write-Host "Updated description for computer $($computer.Name)"
