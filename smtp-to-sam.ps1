baea96602e3f61f20a1b7867a3923ffe7a3b2955
squf <1562592+squf@users.noreply.github.com>
2023-10-06 22:22:23 -0400

    Update smtp-to-sam.ps1

--- smtp-to-sam.ps1
+++ smtp-to-sam.ps1
@@ -1,15 +1,10 @@
-﻿# Import the CSV file containing the email addresses
-$emailList = Import-Csv -Path "C:\scripts\DistroListMembership\Users-To-Add.csv" -Header "Email"
+﻿$emailList = Import-Csv -Path "C:\folder\EXAMPLE.csv" -Header "Email"
 
-# Create an empty array to store the results
 $results = @()
 
-# Loop through each email address and look up the associated SamAccountName
 foreach ($email in $emailList) {
-    # Search Active Directory for the email address
     $adUser = Get-ADUser -Filter {EmailAddress -eq $email.Email} -Properties SamAccountName
     
-    # If a user is found, add the email address and SamAccountName to the results array
     if ($adUser) {
         $results += [PSCustomObject]@{
             SamAccountName = $adUser.SamAccountName
@@ -17,5 +12,4 @@ foreach ($email in $emailList) {
     }
 }
 
-# Export the results to a CSV file
-$results | Export-Csv -Path "C:\scripts\DistroListMembership\SMTP-TO-SAM_CLEANED.csv" -NoTypeInformation
+$results | Export-Csv -Path "C:\folder\EXAMPLE_filtered.csv" -NoTypeInformation
