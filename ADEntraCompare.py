1c82ba6efc2da2a065768c31bcecaa42bfc555df
squf <1562592+squf@users.noreply.github.com>
2024-10-21 10:21:21 -0400

    Create ADEntraCompare.py
    using pandas and xlsxwriter to compare AD to Entra computers, to find mismatches in an environment, useful for checking if your Entra Connection is working properly in HAADJ environments

---
+++ ADEntraCompare.py
@@ -0,0 +1,25 @@
+# this script relies on computer reports existing in a static location
+# update the ad_computers & entra_computers variable locations at the top
+# i have a seperate powershell script called "exportADComputers.ps1" on my github to grab the AD computers report
+# you can export Entra devices in your environment through the GUI at entra.microsoft.com
+import pandas as pd
+
+ad_computers = pd.read_csv(r'C:\reports\AD_Computers.csv')
+entra_computers = pd.read_csv(r'C:\reports\Entra_Computers.csv')
+
+entra_computers.rename(columns={'displayName': 'Name'}, inplace=True)
+
+matches = pd.merge(ad_computers, entra_computers, on='Name', how='inner')
+
+ad_missing_in_entra = ad_computers[~ad_computers['Name'].isin(entra_computers['Name'])]
+
+entra_missing_in_ad = entra_computers[~entra_computers['Name'].isin(ad_computers['Name'])]
+
+output_path = r'C:\users\danielq\documents\reports\computers\Computer_Comparison.xlsx'
+with pd.ExcelWriter(output_path, engine='xlsxwriter') as writer:
+    ad_computers.to_excel(writer, sheet_name='AD Computers', index=False)
+    entra_computers.to_excel(writer, sheet_name='Entra Computers', index=False)
+    matches.to_excel(writer, sheet_name='Matches', index=False)
+    ad_missing_in_entra.to_excel(writer, sheet_name='AD Missing in Entra', index=False)
+
+print(f"Comparison file saved to: {output_path}")
