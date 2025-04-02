49e609694596c8c11c70380edc53a65ec8ae9c50
squf <1562592+squf@users.noreply.github.com>
2025-04-02 10:46:47 -0400

    Update exportADComputers.ps1
    added dir check

--- AD Entra reporting/exportADComputers.ps1
+++ AD Entra reporting/exportADComputers.ps1
@@ -1,1 +1,8 @@
-Get-ADComputer -Filter * -Properties * | Select-Object Name,DistinguishedName,OperatingSystem,Enabled | Export-Csv -Path "C:\reports\AD_Computers.csv" -NoTypeInformation
+# checks if c:\reports exists, creates it if it does not
+$directory = "C:\reports"
+if (-Not (Test-Path $directory)) {
+    New-Item -Path $directory -ItemType Directory
+}
+
+# gets the AD computer info and spits it out to the .csv in c:\reports
+Get-ADComputer -Filter * -Properties * | Select-Object Name,DistinguishedName,OperatingSystem,Enabled | Export-Csv -Path "$directory\AD_Computers.csv" -NoTypeInformation
